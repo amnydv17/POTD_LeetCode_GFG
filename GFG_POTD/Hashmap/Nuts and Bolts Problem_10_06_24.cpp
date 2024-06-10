2496eb6813db0dab450af7ec4800d86d0765748c
Aman Yadav <yadavaman150902@gmail.com>
2024-06-10 20:20:01 +0530

    Solution of the Day

---
+++ GFG_POTD/Hashmap/Nuts and Bolts Problem_10_06_24.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // Aman Yadav
+        char temp[]={'!','#','$','%','&','*','?','@','^'};
+        unordered_map<char,int> mm;
+        int j=0;
+        for(int i=0;i<n;i++)mm[nuts[i]]++;
+        for(int i=0;i<9;i++){
+            if(mm[temp[i]]){
+                nuts[j]=temp[i];
+                bolts[j]=temp[i];
+                j++;
+            }
+        }
+    }
+};
