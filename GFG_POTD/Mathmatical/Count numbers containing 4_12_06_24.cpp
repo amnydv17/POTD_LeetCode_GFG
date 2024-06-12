a7f21911dc0d844860e2e79a65957c03eb253696
Aman Yadav <yadavaman150902@gmail.com>
2024-06-12 16:55:39 +0530

    Solution of the Day

---
+++ GFG_POTD/Mathmatical/Count numbers containing 4_12_06_24.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        // Aman Yadav
+        int ans=0;
+        for(int i=4;i<=n;i++){
+            int j=i;
+            while(j){
+                if(j%10==4){
+                    ans++;
+                    break;
+                }
+                j/=10;
+            }
+        }
+        return ans;
+    }
+};
---
+++ LeetCode_POTD/Array/75. Sort Colors_12_06_24.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        
+        int count_0 = 0;
+        int count_1 = 0;
+        int count_2 = 0;
+        
+        for(int &num : nums) {
+            if(num == 0)
+                count_0++;
+            else if(num == 1)
+                count_1++;
+            else
+                count_2++;
+        }
+        
+        for(int i = 0; i<n; i++) {
+            if(count_0 > 0) {
+                nums[i] = 0;
+                count_0--;
+            } else if(count_1 > 0) {
+                nums[i] = 1;
+                count_1--;
+            } else if(count_2 > 0) {
+                nums[i] = 2;
+                count_2--;
+            }
+        }
+    }
+};
