bc59a286d8302384ce42ce36b0835833b927cfb2
Aman Yadav <yadavaman150902@gmail.com>
2024-07-06 21:18:43 +0530

    Solution of the Day

---
+++ GFG_POTD/Binary Tree/Populate Inorder Successor for all nodes_06_07_24.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+     Node *nxt=NULL;
+    void populateNext(Node *root) {
+        // Aman Yadav
+        if(!root)return;
+        populateNext(root->right);
+        root->next=nxt;
+        nxt=root;
+        populateNext(root->left);
+    }
+};
