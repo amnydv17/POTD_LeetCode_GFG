92eb4601fa3936d2b8aba622913fc19c6238d59f
Aman Yadav <yadavaman150902@gmail.com>
2024-08-10 01:15:07 +0530

    Solution of the Day

---
+++ GFG_POTD/Array/09_08_24_ Maximize Array Value After Rearrangement.cpp
@@ -0,0 +1,18 @@
+## Maximize Array Value After Rearrangement
+
+
+
+class Solution {
+  public:
+    int Maximize(vector<int> &arr) {
+        // Aman Yadav 
+        sort(arr.begin(), arr.end());
+        long long int ans=0;
+        int mod=1e9+7;
+        for(int i=0;i<arr.size();i++){
+            ans+=1LL*i*arr[i];
+            ans%=mod;
+        }
+        return ans;
+    }
+};
