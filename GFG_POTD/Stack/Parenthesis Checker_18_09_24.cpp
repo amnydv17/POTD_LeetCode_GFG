c7e1170a03167af4bae889451a227ab06586951d
Aman Yadav <yadavaman150902@gmail.com>
2024-09-18 22:36:46 +0530

    Solution Of The Day

---
+++ GFG_POTD/Stack/Parenthesis Checker_18_09_24.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Aman Yadav
+        stack<char>st;
+        for(auto i:x){
+            if(i=='('|| i=='[' || i=='{') st.push(i);
+            else{
+                if(st.empty()) return false;
+                if(i==')' && st.top()=='(') st.pop();
+                else if(i==']' && st.top()=='[') st.pop();
+                else if(i=='}' && st.top()=='{') st.pop();
+                else return false;
+            }
+        }
+        if(!st.empty()) return false;
+        return true;
+    }
+
+};
