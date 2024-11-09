290ca877752295ece6af0be8e4e2bdc9c6dd74a1
Aman Yadav <yadavaman150902@gmail.com>
2024-11-10 00:40:49 +0530

    Solution of the Day

---
+++ LeetCode_POTD/Bit/3133. Minimum Array End_09_11_24.cpp
@@ -0,0 +1,18 @@
+//Approach - Simple using AND and OR
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long num = x;
+
+        for(int i = 1; i < n; i++) {
+
+            //num+1
+            num = (num+1) | x;
+            
+        }
+
+        return num;
+    }
+};
