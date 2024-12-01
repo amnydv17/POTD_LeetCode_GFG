bb7bf5a2476fd4e14ba754e770204bd79c737391
Aman Yadav <yadavaman150902@gmail.com>
2024-12-01 22:26:22 +0530

    Solution of the day

---
+++ GFG_POTD/String/Non Repeating Character_01_12_24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+        // Aman Yadav
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]-'a']==1) return s[i];
+        }
+        return '$';
+    }
+};
