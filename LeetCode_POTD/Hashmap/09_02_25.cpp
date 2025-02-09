6f73a5cd2a423dfaffcd7457f16592bd11295674
Aman Yadav <yadavaman150902@gmail.com>
2025-02-10 00:06:26 +0530

    Solution of the Day

---
+++ LeetCode_POTD/Hashmap/09_02_25.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+      long long countBadPairs(vector<int>& nums) {
+          // int n = nums.size();
+  
+          // long long result = 0;
+  
+          // for(int i = 0; i < n; i++) {
+          //     for(int j = i+1; j < n; j++) {
+          //         if(j-i != nums[j] - nums[i]) {
+          //             result++;
+          //         }
+          //     }
+          // }
+  
+          // return result;
+  
+  
+          int n = nums.size();
+  
+          long long result = 0;
+          for(int i = 0; i < n; i++) {
+              nums[i] = nums[i]-i;
+          }
+  
+          unordered_map<int, int> mp;
+          mp[nums[0]] = 1;
+  
+          for(int j = 1; j < n; j++) {
+              int countOfNumsj = mp[nums[j]];
+  
+              int totalNumsBeforej = j;
+  
+              int badPairs = totalNumsBeforej - countOfNumsj;
+  
+              result += badPairs;
+  
+              mp[nums[j]]++;
+          }
+  
+          return result;
+      }
+  };
