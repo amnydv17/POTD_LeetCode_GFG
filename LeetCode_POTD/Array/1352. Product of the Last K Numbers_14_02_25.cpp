67f15ebd2018772f1c8b1a2192a69c8bc17370d5
Aman Yadav <yadavaman150902@gmail.com>
2025-02-14 22:18:25 +0530

    Solution of the Day

---
+++ LeetCode_POTD/Array/1352. Product of the Last K Numbers_14_02_25.cpp
@@ -0,0 +1,34 @@
+class ProductOfNumbers {
+  public:
+      vector<int> nums; //cumulative prod store karenge
+      int n;
+  
+      ProductOfNumbers() {
+          nums.clear();
+          n = 0;
+      }
+      
+      void add(int num) {
+          if(num == 0) {
+              nums = {};
+              n = 0;
+          } else {
+              if(nums.empty()) {
+                  nums.push_back(num);
+              } else {
+                  nums.push_back(nums[n-1] * num);
+              }
+              n++; //increasing size by 1
+          }
+      }
+      
+      int getProduct(int k) {
+          if(k > n) {
+              return 0;
+          } else if(k == n) {
+              return nums[n-1];
+          }
+  
+          return nums[n-1] / nums[n-k-1];
+      }
+  };
