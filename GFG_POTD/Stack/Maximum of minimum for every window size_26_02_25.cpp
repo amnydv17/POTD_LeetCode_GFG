3f987515a648406d484c915f1af7b3fb7c9b82ca
Aman Yadav <yadavaman150902@gmail.com>
2025-02-26 22:42:59 +0530

    Solution of the Day

---
+++ GFG_POTD/Stack/Maximum of minimum for every window size_26_02_25.cpp
@@ -0,0 +1,66 @@
+class Solution {
+  public:
+    vector<int> getPreSmall(vector<int>& arr, int n) {
+        stack<int> st;
+        vector<int> x(n, -1);
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                x[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        return x;
+    }
+
+    
+    vector<int> getNextSmall(vector<int>& arr, int n) {
+        stack<int> st;
+        vector<int> y(n, n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                y[i] = st.top();
+            }
+
+            st.push(i);
+        }
+
+        return y;
+    }
+
+    
+    vector<int> maxOfMins(vector<int>& arr) {
+        //Aman Yadav
+        int n = arr.size();  
+
+        vector<int> x = getPreSmall(arr, n);    // x is prevSmaller
+        vector<int> y = getNextSmall(arr, n);    // y is nextsmaller
+        vector<int> ans(n, 0);
+
+        
+        for (int i = 0; i < n; i++) {
+            int len = y[i] - x[i] - 1;
+            ans[len - 1] = max(ans[len - 1], arr[i]);
+        }
+
+        
+        for (int i = n - 2; i >= 0; i--) {
+            ans[i] = max(ans[i], ans[i + 1]);
+        }
+
+        return ans;
+        
+        
+    }
+};
